6fbd36f2a923fa2dff84dcea0532be489fbbdc81
VadLusk <AdLusk@gmail.com>
2018-09-08 11:24:28 -0600

    Add .rows[0] to controller.

--- models/User.js
+++ models/User.js
@@ -4,6 +4,10 @@ class User {
   static all() {
     return database.raw('SELECT * FROM users');
   }
+
+  static find(id) {
+    return database.raw('SELECT * FROM users WHERE id = ?', [id]);
+  }
 }
 
 module.exports = User
