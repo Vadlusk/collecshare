542c66343108d9afde8aa6ea09072c2f06a0b049
VadLusk <AdLusk@gmail.com>
2018-09-07 21:26:18 -0600

    Add usersController.

--- controllers/api/v1/usersController.js
+++ controllers/api/v1/usersController.js
@@ -1,5 +1,8 @@
-const index = (req, res, next) => {
+const User = require('../../../models/User');
 
+const index = (req, res, next) => {
+  User.all()
+    .then((users) => res.json(users));
 };
 
 module.exports = { index };
