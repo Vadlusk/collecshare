e40a20a366d6d2984738dbb7dcc0ea56c394b8f6
VadLusk <AdLusk@gmail.com>
2018-09-08 17:50:22 -0600

    Add delete user test.

--- test/users/delete.test.js
+++ test/users/delete.test.js
@@ -1,5 +1,24 @@
 var config = require('../test_helper');
 
 describe('DELETE /api/v1/users/:id', () => {
-
+  it('should delete a user', done => {
+    config.chai.request(config.app)
+      .delete('/api/v1/users/1')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('message');
+        res.body.message.should.equal('Successfully deleted user 1');
+        done();
+      });
+  });
+  it('should return 404 if user id does not exist', done => {
+    config.chai.request(config.app)
+      .delete('/api/v1/users/999999')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
 });
