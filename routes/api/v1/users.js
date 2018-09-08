1ec2877d21942629b2dd1a205fe61c6ab7353289
VadLusk <AdLusk@gmail.com>
2018-09-08 11:17:43 -0600

    Add show route.

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -3,5 +3,6 @@ var router = express.Router();
 const usersController = require('../../../controllers/api/v1/usersController');
 
 router.get('/', usersController.index);
+router.get('/:id', usersController.show);
 
 module.exports = router;
