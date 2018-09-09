4ed38ccb995ad6668719564c02819e7059298e63
VadLusk <AdLusk@gmail.com>
2018-09-08 18:17:26 -0600

    Add route to users router.

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const usersController = require('../../../controllers/api/v1/usersController');
 
+router.post('/', usersController.create);
 router.get('/', usersController.index);
 router.get('/:id', usersController.show);
 router.delete('/:id', usersController.destroy);
