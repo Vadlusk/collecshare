991f7b8af188b8d875e09ad2b7de9bc47b6b81b5
VadLusk <AdLusk@gmail.com>
2018-09-08 18:14:22 -0600

    Add create user test.

---
+++ test/users/create.test.js
@@ -0,0 +1,23 @@
+var config = require('../test_helper');
+
+describe('POST /api/v1/users', () => {
+  it('should create a user with only uid and name', done => {
+    config.chai.request(config.app)
+      .post('/api/v1/users')
+      .send({uid: 1, username: 'New user'})
+      .end((err, res) => {
+        res.should.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('id');
+        res.body.id.should.equal(21);
+        res.body.should.have.property('uid');
+        res.body.user_id.should.equal(1);
+        res.body.should.have.property('username');
+        res.body.should.have.property('photo_url');
+        res.body.should.have.property('location');
+        res.body.should.have.property('bio');
+        done();
+      });
+  });
+});
