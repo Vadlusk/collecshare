be0f3c57ab45b759c345d451a417e2e6c0d30b9a
VadLusk <AdLusk@gmail.com>
2018-09-10 11:05:38 -0600

    Add three sad path tests.

--- test/users/create.test.js
+++ test/users/create.test.js
@@ -53,4 +53,29 @@ describe('POST /api/v1/users', () => {
       });
     });
   });
+  context('should not create a user without', () => {
+    it('anything', done => {
+      config.chai.request(config.app)
+      .post('/api/v1/users')
+      .end((err, res) => {
+        res.should.have.status(400);
+      });
+    });
+    it('uid', done => {
+      config.chai.request(config.app)
+      .post('/api/v1/users')
+      .send({uid: 4})
+      .end((err, res) => {
+        res.should.have.status(400);
+      });
+    });
+    it('username', done => {
+      config.chai.request(config.app)
+      .post('/api/v1/users')
+      .send({username: 'Bad User'})
+      .end((err, res) => {
+        res.should.have.status(400);
+      });
+    });
+  });
 });
