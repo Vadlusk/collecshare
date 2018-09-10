79162957edbf1133b1e9b3e19fc117784b01e7a2
VadLusk <AdLusk@gmail.com>
2018-09-10 10:57:07 -0600

    Add sanitization method to user model.

--- models/User.js
+++ models/User.js
@@ -2,6 +2,9 @@ const database = require('../db/config');
 
 class User {
   static create(info) {
+    console.log(info.bio);
+    sanitizeInfo(info);
+    console.log(info.bio);
     return database.raw(
       'INSERT INTO users (uid, username, photo_url, location, bio) VALUES (?, ?, ?, ?, ?) RETURNING *',
       [info.uid, info.username, info.photo_url, info.location, info.bio]
@@ -20,5 +23,12 @@ class User {
     return database.raw('DELETE FROM users WHERE id = ?', [id]);
   }
 }
+
+const sanitizeInfo = info => {
+  if (info.photo_url == undefined) info.photo_url = 'http://shec-labs.com/wp-content/themes/creativemag/images/default.png';
+  if (info.location == undefined) info.location = null;
+  if (info.bio == undefined) info.bio = null;
+  return info;
+};
 
 module.exports = User
