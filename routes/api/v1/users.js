8167e1a10e1e3f4c051237da0dd5d096fa2b5e76
VadLusk <AdLusk@gmail.com>
2018-09-11 17:22:47 -0600

    Add route.

--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -5,6 +5,7 @@ const usersController = require('../../../controllers/api/v1/usersController');
 router.post('/', usersController.create);
 router.get('/', usersController.index);
 router.get('/:uid', usersController.show);
+router.put('/:uid', usersController.update);
 router.delete('/:uid', usersController.destroy);
 
 module.exports = router;
