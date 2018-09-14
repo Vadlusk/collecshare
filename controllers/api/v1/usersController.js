2371c0f80d5cd21792ffc00d2f3b4363d71da1ec
VadLusk <AdLusk@gmail.com>
2018-09-13 21:39:05 -0600

    Add storage config for multer.

--- controllers/api/v1/usersController.js
+++ controllers/api/v1/usersController.js
@@ -1,7 +1,5 @@
 const User    = require('../../../models/User');
 const helpers = require('../../helpers');
-const multer  = require('multer');
-const upload  = multer({ dest: '/avatars/' });
 
 const create = (req, res, next) => {
   if (!req.body.username || !req.body.uid) {
@@ -23,7 +21,7 @@ const show = (req, res, next) => {
 };
 
 const update = (req, res, next) => {
-  console.log(req.body)
+  console.log(req.file)
   User.update(req.body, req.params.uid)
     .then(user => helpers.sendJSON(user, 200, res));
 };
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,11 +1,21 @@
-var express = require('express');
-var router = express.Router();
+var express           = require('express');
+var router            = express.Router();
 const usersController = require('../../../controllers/api/v1/usersController');
+const multer  = require('multer');
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'avatars/')
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + file.originalname)
+  }
+});
+const upload  = multer({ storage });
 
 router.post('/', usersController.create);
 router.get('/', usersController.index);
 router.get('/:uid', usersController.show);
-router.put('/:uid', usersController.update);
+router.put('/:uid', upload.single('avatar'), usersController.update);
 router.delete('/:uid', usersController.destroy);
 
 module.exports = router;
