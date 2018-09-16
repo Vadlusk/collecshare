a1bb641d40ff644b84320307952ea1ab48496d18
VadLusk <AdLusk@gmail.com>
2018-09-16 17:37:17 -0600

    Add sql query.

--- controllers/api/v1/searchController.js
+++ controllers/api/v1/searchController.js
@@ -2,7 +2,7 @@ const AllTables = require('../../../models/AllTables');
 
 const create = (req, res, next) => {
   AllTables.search(req.body.keyword)
-    .then(results => res.json(results));
+    .then(results => console.log(results.rows));
 };
 
 module.exports = { create };
--- models/AllTables.js
+++ models/AllTables.js
@@ -1,8 +1,15 @@
 const database = require('../db/config');
 
 class AllTables {
-  static search() {
-
+  static search(query) {
+    query = '%' + query + '%'
+    return database.raw(
+      `SELECT c.*, u.*
+       FROM collections c
+       JOIN users u ON c.uid = u.uid
+       WHERE title LIKE ?
+       OR description LIKE ?
+       OR username LIKE ?`, [query, query, query])
   }
 }
 
