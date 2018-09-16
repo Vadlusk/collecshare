0333933010bf54e14c6fa85e7f1f7d067bc5c1af
VadLusk <AdLusk@gmail.com>
2018-09-16 00:39:18 -0600

    Refactor test more.

--- test/collections/create.test.js
+++ test/collections/create.test.js
@@ -3,65 +3,53 @@ var config = require('../test_helper');
 describe('POST /api/v1/collections', () => {
   context('should create a collection', () => {
     it('with all required parameters', done => {
-      config.chai.request(config.app)
-      .post('/api/v1/collections')
-      .send({uid: '1', category: 'comics', title: 'New'})
-      .end((err, res) => {
-        res.should.have.status(201);
-        res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.have.property('id');
-        res.body.id.should.equal(21);
-        res.body.should.have.property('uid');
-        res.body.uid.should.equal('1');
-        res.body.should.have.property('category');
-        res.body.category.should.equal('comics');
-        res.body.should.have.property('title');
-        res.body.title.should.equal('New');
-        res.body.should.have.property('description');
-        done();
-      });
+      postToCollections('reqd', done, null);
     });
     it('with all required and optional parameters', done => {
-      config.chai.request(config.app)
-      .post('/api/v1/collections')
-      .send({uid: '1', category: 'comics', title: 'New', description: 'Pretty good'})
-      .end((err, res) => {
-        res.should.have.status(201);
-        res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.have.property('id');
-        res.body.id.should.equal(21);
-        res.body.should.have.property('uid');
-        res.body.uid.should.equal('1');
-        res.body.should.have.property('category');
-        res.body.category.should.equal('comics');
-        res.body.should.have.property('title');
-        res.body.title.should.equal('New');
-        res.body.should.have.property('description');
-        res.body.description.should.equal('Pretty good');
-        done();
-      });
+      postToCollections('optional', done, 'Pretty good');
     });
   });
   context('should not create a collection without', () => {
-    it('anything', done => sendWithout('anything', done));
-    it('a uid', done => sendWithout('uid', done));
-    it('a category', done => sendWithout('category', done));
-    it('a title', done => sendWithout('title', done));
+    it('anything', done => postToCollections('anything', done, null));
+    it('a uid', done => postToCollections('uid', done, null));
+    it('a category', done => postToCollections('category', done, null));
+    it('a title', done => postToCollections('title', done, null));
   });
 });
 
-const sendWithout = (missingItem, done) => {
-  let payload = determinePayload(missingItem);
+const collectionAssertions = (res, status, id, uid, category, title, desc) => {
+  res.should.have.status(status);
+  res.should.be.json;
+  res.body.should.be.a('object');
+  res.body.should.have.property('id');
+  res.body.should.have.property('uid');
+  res.body.should.have.property('category');
+  res.body.should.have.property('title');
+  res.body.should.have.property('description');
+  res.body.id.should.equal(id);
+  res.body.uid.should.equal(uid);
+  res.body.category.should.equal(category);
+  res.body.title.should.equal(title);
+  res.body.description ? res.body.description.should.equal(desc) : null;
+};
+
+const createCollectionErrorAssertions = res => {
+  res.should.have.status(400);
+  res.should.be.json;
+  res.body.should.have.property('error');
+  res.body.error.should.equal('uid, category, title required');
+};
+
+const postToCollections = (situation, done, desc) => {
   config.chai.request(config.app)
   .post('/api/v1/collections')
-  .send(payload)
+  .send(determinePayload(situation))
   .end((err, res) => {
-    res.should.have.status(400);
-    res.should.be.json;
-    res.body.should.have.property('error');
-    res.body.error.should.equal('uid, category, title required');
+    if (res.status === 400) {
+      createCollectionErrorAssertions(res);
+    } else {
+      collectionAssertions(res, 201, 21, '1', 'comics', 'New', desc);
+    }
     done();
   });
 };
@@ -77,6 +65,13 @@ const determinePayload = item => {
       break;
     case 'title':
       payload = { uid: '1', category: 'comics' }
+      break;
+    case 'reqd':
+      payload = { uid: '1', category: 'comics', title: 'New' }
+      break;
+    case 'optional':
+      payload = { uid: '1', category: 'comics', title: 'New',
+        description: 'Pretty good' }
       break;
     default:
       payload = {}
