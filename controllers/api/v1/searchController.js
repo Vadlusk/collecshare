52fdb929eb41228aeef41d017c9f4786d617d5ce
VadLusk <AdLusk@gmail.com>
2018-09-16 16:32:11 -0600

    Add all tables model.

--- controllers/api/v1/searchController.js
+++ controllers/api/v1/searchController.js
@@ -1,8 +1,8 @@
 const AllTables = require('../../../models/AllTables');
 
 const create = (req, res, next) => {
-  AllTables.Search(req.body.keyword)
-    .then(results => res.json(results);
+  AllTables.search(req.body.keyword)
+    .then(results => res.json(results));
 };
 
 module.exports = { create };
---
+++ models/AllTables.js
@@ -0,0 +1,9 @@
+const database = require('../db/config');
+
+class AllTables {
+  static search() {
+
+  }
+}
+
+module.exports = AllTables;
