84d2156c3adfb451e2ef7ac74fe991b5a17701ca
VadLusk <AdLusk@gmail.com>
2018-09-17 08:41:08 -0600

    Add console.log.

--- controllers/api/v1/usersController.js
+++ controllers/api/v1/usersController.js
@@ -23,6 +23,7 @@ const show = (req, res, next) => {
 
 const update = (req, res, next) => {
   if (req.file) req.body.avatar = req.file.path;
+  console.log(req.body);
   User.update(req.body, req.params.uid)
     .then(user => helpers.sendJSON(user, 200, res));
 };
