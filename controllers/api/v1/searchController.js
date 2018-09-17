ca96fecc4b147fa2b4a24cace0c1bb0aa5a48674
VadLusk <AdLusk@gmail.com>
2018-09-17 08:40:29 -0600

    Add to switch to master.

--- controllers/api/v1/searchController.js
+++ controllers/api/v1/searchController.js
@@ -2,7 +2,7 @@ const AllTables = require('../../../models/AllTables');
 
 const create = (req, res, next) => {
   AllTables.search(req.body.keyword)
-    .then(results => console.log(results.rows));
+    .then(results => res.json(results.rows));
 };
 
 module.exports = { create };
--- test/search/create.test.js
+++ test/search/create.test.js
@@ -8,11 +8,17 @@ describe('POST /api/v1/search', () => {
       .end((err, res) => {
         res.should.have.status(200);
         res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.have.property('collections');
-        res.body.should.have.property('users');
-        res.body['collections'].should.be.a('array');
-        res.body['users'].should.be.a('array');
+        res.body.should.be.a('array');
+        res.body.length.should.equal(20);
+        res.body[0].should.have.property('title');
+        res.body[0].should.have.property('category');
+        res.body[0].should.have.property('image');
+        res.body[0].should.have.property('description');
+        res.body[0].should.have.property('user');
+        res.body[0].user.should.be.a('object');
+        res.body[0].user.should.have.property('username');
+        res.body[0].user.should.have.property('avatar');
+        res.body[0].user.should.have.property('bio');
         done();
       });
   });
