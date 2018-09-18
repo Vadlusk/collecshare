4a7d1c0392c78b596b3b88a0b0660be9a21541d4
VadLusk <AdLusk@gmail.com>
2018-09-18 10:05:24 -0600

    Change multer argument to null.

--- multer.js
+++ multer.js
@@ -4,7 +4,7 @@ const fileFilter = (req, file, cb) => {
   if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
     cb(null, true);
   } else {
-    cb(new Error('File upload unsuccessful'), false);
+    cb(null, false);
   }
 };
 
