79ec2bf1b086404fff0e92a10123885409f3ecaa
VadLusk <AdLusk@gmail.com>
2018-09-18 10:29:21 -0600

    Add content to items index.

--- controllers/api/v1/itemsController.js
+++ controllers/api/v1/itemsController.js
@@ -2,7 +2,8 @@ const Item = require('../../../models/Item');
 const helpers = require('../../helpers');
 
 const index = (req, res, next) => {
-
+  Item.all().
+    then(items => res.json(items));
 };
 
 module.exports = { index };
