c0b215d35efd5eddf4cdd91dd2defeba62f86c5b
VadLusk <AdLusk@gmail.com>
2018-09-18 10:23:06 -0600

    Add items index test.

--- test/items/index.test.js
+++ test/items/index.test.js
@@ -1,27 +1,26 @@
-// var config = require('../test_helper');
-//
-// describe('GET /api/v1/items', () => {
-//   it('should return all the users', done => {
-//     config.chai.request(config.app)
-//       .get('/api/v1/users')
-//       .end((err, res) => {
-//         res.should.have.status(200);
-//         res.should.be.json;
-//         res.body.should.be.a('array');
-//         res.body.length.should.equal(20);
-//         res.body[0].should.have.property('id');
-//         res.body[0].id.should.equal('1');
-//         res.body[0].should.have.property('collection_id');
-//         res.body[0].collection_id.should.equal('1');
-//         res.body[0].should.have.property('image');
-//         res.body[0].avatar.should.equal(
-//           'http://shec-labs.com/wp-content/themes/creativemag/images/default.png'
-//         );
-//         res.body[0].should.have.property('location');
-//         res.body[0].location.should.equal('Denver, CO');
-//         res.body[0].should.have.property('bio');
-//         res.body[0].bio.should.equal('I\'m pretty cool.');
-//         done();
-//       });
-//   });
-// });
+var config = require('../test_helper');
+
+describe('GET /api/v1/items', () => {
+  it('should return all the items', done => {
+    config.chai.request(config.app)
+      .get('/api/v1/items')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('array');
+        res.body.length.should.equal(20);
+        res.body[0].should.have.property('id');
+        res.body[0].id.should.equal('1');
+        res.body[0].should.have.property('collection_id');
+        res.body[0].collection_id.should.equal('1');
+        res.body[0].should.have.property('image');
+        res.body[0].should.have.property('value');
+        res.body[0].value.should.equal('5.00');
+        res.body[0].should.have.property('title');
+        res.body[0].title.should.equal('Item');
+        res.body[0].should.have.property('description');
+        res.body[0].description.should.equal('Cool thing I have');
+        done();
+      });
+  });
+});
