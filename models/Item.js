7b632fc951a1d77c4769cff0a81c59b14dc61066
VadLusk <AdLusk@gmail.com>
2018-09-18 18:31:58 -0600

    Add create method to item model.

--- models/Item.js
+++ models/Item.js
@@ -1,6 +1,16 @@
 const database = require('../db/config');
+const helpers  = require('./helpers');
 
 class Item {
+  static create(info) {
+    console.log(info)
+    helpers.sanitizeInfo(info, 'item');
+    let q = `INSERT INTO items (collection_id, title, value, description, image)
+             VALUES (?, ?, ?, ?, ?) RETURNING *`
+    return database.raw(q, [info.collection_id, info.title, info.value,
+      info.description, info.image]);
+  }
+
   static all() {
     return database.raw('SELECT * FROM items');
   }
